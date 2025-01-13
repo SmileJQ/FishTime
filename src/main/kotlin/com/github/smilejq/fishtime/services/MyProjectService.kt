e2fb99fb57d39057041279e6d0a316735896fc9b
GitHub Action <action@github.com>
2025-01-13 08:15:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/smilejq/fishtime/services/MyProjectService.kt
+++ src/main/kotlin/com/github/smilejq/fishtime/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.smilejq.fishtime.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.smilejq.fishtime.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
